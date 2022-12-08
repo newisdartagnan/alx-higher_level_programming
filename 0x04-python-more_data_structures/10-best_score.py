52db6c05187065d8aa851ed83a071cedb03951ab
newisdartagnan <newisboris@gmail.com>
2022-12-07 18:29:20 -0800

    all more structure

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    array = list(a_dictionary.keys())[0]
+    biggest = a_dictionary[array]
+    for x, y in a_dictionary.items():
+        if y > biggest:
+            biggest = y
+            array = x
+    return (array)
