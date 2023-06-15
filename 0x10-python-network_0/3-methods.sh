95d8c653ebcce3b745be1b6747bda980088d8026
newisdartagnan <borisikula@gmail.com>
2023-06-14 19:47:33 -0700

    commit 2

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Takes a URL and ndisplays all HTTP methods server accepts
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
 curl -sI "$1" | grep Allow: | cut -d ' ' -f 2-
