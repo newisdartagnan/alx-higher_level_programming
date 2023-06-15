4f9572a8588038cc6a5f1dcd0c2f6606c14fa3e1
newisdartagnan <borisikula@gmail.com>
2023-06-14 20:02:00 -0700

    commit 3

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using requests package"""
+"""Fetches https://alx-intranet.hbtn.io/status using requests package"""
 if __name__ == "__main__":
     import requests
 
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:\n"
           "\t- type: {}\n"
           "\t- content: {}".format(type(r.text), r.text))
