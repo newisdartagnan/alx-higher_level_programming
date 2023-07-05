feb50cfadd67d1aa2a2599cba16efb43fba65873
Boris Ikula <111108765+newisdartagnan@users.noreply.github.com>
2023-07-05 20:26:48 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-# Takes a URL and ndisplays all HTTP methods server accepts
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
+
+# Check if URL argument is provided
+if [[ -z $1 ]]; then
+  echo "Error: URL argument is missing."
   exit 1
 fi
-curl -sI "$1" | grep Allow: | cut -d ' ' -f 2-
+
+# Make a request to the URL and retrieve the allowed methods
+response=$(curl -sI -X OPTIONS "$1" | grep -i "Allow:" | sed 's/Allow: //i')
+
+# Check if any allowed methods are found
+if [[ -z $response ]]; then
+  echo "No allowed methods found for $1"
+else
+  echo "Allowed methods for $1: $response"
+fi
+
